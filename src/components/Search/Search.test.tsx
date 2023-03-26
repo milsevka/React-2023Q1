feda2c99bba59eaffab70262d75b0174eb58b1a0
Milsevka <lusindami99@gmail.com>
2023-03-26 22:05:19 +0300

    fis: search tests

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -3,8 +3,8 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Search } from './Search';
 
-describe('render form', () => {
-  it('render form', () => {
+describe('render search', () => {
+  it('render search input', () => {
     render(<Search />);
     const textInput = screen.getByRole('textbox');
     userEvent.type(textInput, 'Kisa');
